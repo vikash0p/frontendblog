3c7c3ce5ed0e783591ee9bb4a47e88e194d39aa4
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-09-21 12:55:01 +0530

    solve the error in contact page

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -5,13 +5,13 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Contact Us | Artist Aura",
   description:
-    "Get in touch with the Artist Aura team. Whether you have questions, feedback, or collaboration ideas, we'd love to hear from you. Reach out to us through our contact form or social media.",
+    "Get in touch with the Artist Aura team. Whether you have questions, feedback, or collaboration ideas, we have love to hear from you. Reach out to us through our contact form or social media.",
   openGraph: {
     title: "Contact Us | Artist Aura",
     description:
       "Have a question or a creative idea to share? Contact the Artist Aura team today. We're here to connect, collaborate, and help you on your artistic journey.",
-    url: "https://artist-aura-blog.vercel.app/contact", // Update with your actual Contact page URL
-    images: "/public/images/contact-banner.png", // Path to your Contact page image
+    url: "https://artist-aura-blog.vercel.app/contact",
+    images: "/public/images/contact-banner.png",
     siteName: "Artist Aura",
   },
   twitter: {
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     title: "Contact Us | Artist Aura",
     description:
       "We'd love to hear from you! Reach out to Artist Aura with your questions, feedback, or partnership inquiries.",
-    images: "/public/images/contact-banner.png", // Path to your Contact page image for Twitter
+    images: "/public/images/contact-banner.png",
   },
 };
 
